2299761c3256a5d3b8175046ccd279332eb122e9
Helloworldist <33469961+Helloworldist@users.noreply.github.com>
2018-07-01 13:12:40 -0700

    분석 파일 - 값 입출력

--- analysis.py
+++ analysis.py
@@ -1,41 +1,53 @@
-from testcase import L_testcase as lt
-from testcase import R_testcase as rt
 import matplotlib.pyplot as plt
 import numpy as np
 
 def run():
-    loc_LFlist = []
-    loc_RFlist = []
-    tmp_l = [0,0,0,0,0]
-    tmp_r = [0,0,0,0,0]
-    tstIndex = 10
+    # File Input
+    streamf=open('stream.txt', 'r')
+    data = [int(num) for num in streamf.read().split()]
+    streamf.close()
 
-    for i in range(1, tstIndex):
-        loc_LFlist.append(lt())
-        loc_RFlist.append(rt())
+    print(len(data))
+    print(data)
 
-    try:
+    # The List as a stack for press value
+    tmp_l = [0, 0, 0, 0, 0]
+    tmp_r = [0, 0, 0, 0, 0]
+    IndexOfData = len(data) / 10
+
+    for j in range(0, int(len(data)/10)):
         for i in range(0, 5):
-            for j in range(0, len(loc_LFlist)):
-                tmp_l[i]+=loc_LFlist[j][i]
-                tmp_r[i]+=loc_RFlist[j][i]
-    except:
-        pass
+            tmp_l[i]+=data[i+(j*10)]
+            tmp_r[i]+=data[i+((j*10)+5)]
 
-    print(loc_LFlist)
-    print(loc_RFlist)
-
+    print(tmp_l, tmp_r)
     print("=======================")
 
     for i in range(0, len(tmp_l)):
-        tmp_l[i]/=tstIndex
-        tmp_r[i]/=tstIndex
+        tmp_l[i]/=IndexOfData
+        tmp_r[i]/=IndexOfData
 
     print("Average of LFoot : ", tmp_l)
     print("Average of RFoot : ", tmp_r)
 
+    out_l=[]
+    out_r=[]
+
+    for tmp_ls in range(0,len(tmp_l)):
+       out_l.append(int(tmp_l[tmp_ls]))
+    for tmp_rs in range(0,len(tmp_r)):
+       out_r.append(int(tmp_r[tmp_rs]))
+
+    print(out_l+out_r)
+
+    f=open('value.txt', 'w')
+    f.write(' '.join(str(out_l)))
+    f.write(' '.join(str(out_r)))
+    f.close()
+
     return tmp_l+tmp_r
 
+# Visualize step
 def visualize(pressure):
     type=['LFoot1', 'LFoot2', 'LFoot3', 'LFoot4', 'LFoot5',
           'RFoot1', 'RFoot2', 'RFoot3', 'RLFoot4', 'RFoot5']
@@ -54,8 +66,13 @@ def visualize(pressure):
 
     plt.ylabel('Pressure Value')
     plt.xlabel('The pressure of Foot')
-    plt.show()
     plt.savefig('result.png')
 
+def contents(data):
+    pass
 
+
+
+# Execute
 visualize(run())
+#contents(run())
