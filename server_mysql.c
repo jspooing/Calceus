c09847f938cb41064945330097ab10a64ee884e0
CHC <jspooing@naver.com>
2018-07-06 16:37:38 +0900

    DB select

--- server_mysql.c
+++ server_mysql.c
@@ -9,6 +9,15 @@
 #define CHOP(x) x[strlen(x)-1] = NULL
 
 
+typedef struct _Matchtable{
+	char user[15];
+	char designer[15];
+	int size,num;
+	char wide[5];
+	char color[15];
+	char detail[200];
+	char sex;
+}Match;
 
 int DBinsert(char* table , char data[][100], int nData){
 
@@ -149,4 +158,71 @@ int DBcheck(char * table, char * column, char * value){
 	else return 1;
 
 
+}
+
+int DBselect_match(char * buf,char *column ,char* value, int dnum){
+	MYSQL *connection  = NULL, conn;
+	MYSQL_RES *sql_result;
+	MYSQL_ROW sql_row;
+
+	int query_stat;
+	int stat =0;
+	char query[255];
+	char correct[15];
+	int i;
+
+
+	printf("DB_select : ");
+	fflush(stdout);
+	mysql_init(&conn);
+	connection = mysql_real_connect(&conn, DB_HOST, DB_USER, DB_PASS, DB_NAME, 3306, (char *)NULL, 0);
+
+	if(connection == NULL)
+	{
+		fprintf(stderr, "Mysql connection error : %s", mysql_error(&conn));
+		return -1;
+	}
+
+	sprintf(query,"select * from DU_MATCH where %s = '%s'",column,value);
+
+	printf("%s",query);
+	fflush(stdout);
+
+	query_stat = mysql_query(connection,query);
+
+	if(query_stat != 0)
+	{
+
+		fprintf(stderr, "Mysql query error : %s", mysql_error(&conn));
+		return -1;
+	}
+
+
+
+	sql_result = mysql_store_result(connection);
+
+
+	printf("sql_result : loaded \n");
+
+
+
+
+
+
+
+
+	while((	sql_row = mysql_fetch_row(sql_result))!=NULL){
+
+		for(i=0; i < dnum; i++)
+			sprintf(buf,"%s%s/",buf,sql_row[i]);	
+		sprintf(buf,"%s#",buf);
+		
+
+
+
+		fflush(stdout);
+	}
+	
+	printf("%s",buf);
+	return 0;
 }
